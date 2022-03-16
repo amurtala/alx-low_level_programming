f939668ff78bcf5f6981a7c5480f701b6f55167a
amurtala <amurtala@gmail.com>
2022-03-15 20:47:38 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,8 +2,10 @@
 
 /**
  * main - print _putchar, followed by a new line
- * the program should return 0.
- * Return: Always 0 (Success)
+ *
+ * the program should return
+ *
+ * Return: 0 (Success)
  */
 
 int main(void)
@@ -11,11 +13,12 @@ int main(void)
 	char text[9] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i > 8; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(text[i]);
 	}
 	_putchar('\n');
+
 	return (0);
 }
 	
