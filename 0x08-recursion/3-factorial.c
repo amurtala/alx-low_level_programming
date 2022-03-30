17bd2bd87b61b11ac8c673bfca8987e9caa328c7
amurtala <amurtala@gmail.com>
2022-03-30 14:28:31 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,7 +1,11 @@
 #include "main.h"
+#include <stddef.h>
+
 /**
+ * factorial - return factorial
  * @n: input
- * Return: Always 0 (Success)
+ *
+ * Return: Always 0 (Success), -1 on error
  */
 int factorial(int n)
 {
@@ -11,4 +15,5 @@ int factorial(int n)
 		return (1);
 	else
 		return (n * factorial(n - 1));
+	return (1);
 }
