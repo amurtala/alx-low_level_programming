fc717f93631d83083871a19fd6c362fb2cd032ba
amurtala <amurtala@gmail.com>
2022-03-30 14:15:57 -0700

    3-factorial.c

--- 0x08-recursion/main.c
+++ 0x08-recursion/main.c
@@ -1,7 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-	_print_rev_recursion("\nMuritala Gbadamosi Atanda");
+	int r;
+
+	r = factorial(1);
+	printf("%d\n", r);
+	r = factorial(5);
+	printf("%d\n", r);
+	r = factorial(10);
+	printf("%d\n", r);
+	r = factorial(-1024);
+	printf("%d\n", r);
 	return (0);
 }
