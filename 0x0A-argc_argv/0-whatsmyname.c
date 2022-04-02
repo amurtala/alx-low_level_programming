eb59df989ae400a87a8e2121757345320cf46286
amurtala <amurtala@gmail.com>
2022-04-01 17:03:45 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that print its name
+ * @argc: argument count
+ * @argv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
